e02b3e041d1cd3a0b533b7d13008336ff67f0fea
tseincman <tseincman@gmail.com>
2023-07-03 09:33:05 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/tmseincman/crud/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tmseincman.crud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tmseincman.crud.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
