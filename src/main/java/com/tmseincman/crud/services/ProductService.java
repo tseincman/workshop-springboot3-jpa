d80c62b03738466258908013efef538b01a1d623
tseincman <tseincman@gmail.com>
2023-07-03 14:51:44 -0300

    Product

---
+++ src/main/java/com/tmseincman/crud/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.tmseincman.crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tmseincman.crud.entities.Product;
+import com.tmseincman.crud.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
