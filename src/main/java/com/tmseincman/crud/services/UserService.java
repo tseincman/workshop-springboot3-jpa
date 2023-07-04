d002a17abc8cac465d9a1299ea6ce9e66190392a
tseincman <tseincman@gmail.com>
2023-07-04 14:08:16 -0300

    Exception handling - findById

--- src/main/java/com/tmseincman/crud/services/UserService.java
+++ src/main/java/com/tmseincman/crud/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.tmseincman.crud.entities.User;
 import com.tmseincman.crud.repositories.UserRepository;
+import com.tmseincman.crud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
